4ef6a84465ef36bdddcd96a64e4b8b1dda26ddc2
byattj <byattj@lmax.com>
2021-12-02 09:56:04 +0000

    problem one

---
+++ src/main/java/net/digihippo/aoc/One.java
@@ -0,0 +1,95 @@
+package net.digihippo.aoc;
+
+import java.awt.event.WindowStateListener;
+import java.io.*;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class One
+{
+    public static int countIncreases(InputStream input) throws IOException {
+
+        final IncreaseCounter counter = new IncreaseCounter();
+        processLines(input, counter);
+
+        return counter.increases;
+    }
+
+    public static int countSumIncreases(InputStream inputStream) throws IOException {
+
+        final SumIncreaseCounter counter = new SumIncreaseCounter();
+        processLines(inputStream, counter);
+
+        return counter.increases();
+    }
+
+    private static void processLines(InputStream inputStream, Consumer<String> callback) throws IOException {
+        try (final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                callback.accept(line);
+            }
+        }
+    }
+
+    private static final class IncreaseCounter implements Consumer<String>
+    {
+        private int increases = 0;
+        private int lastDepth = Integer.MAX_VALUE;
+
+        @Override
+        public void accept(String line) {
+            final int depth = Integer.parseInt(line);
+            if (depth > lastDepth) {
+                increases++;
+            }
+            lastDepth = depth;
+        }
+    }
+
+    private static final class Window
+    {
+        private int increases = 0;
+
+        private final int[] window = new int[3];
+        private int index = 0;
+        private boolean full = false;
+
+        public void accept(int depth) {
+            if (!full)
+            {
+                window[index++] = depth;
+                if (index == 3)
+                {
+                    full = true;
+                }
+            }
+            else
+            {
+                int previousSum = Arrays.stream(window).sum();
+                window[index++] = depth;
+                int currentSum = Arrays.stream(window).sum();
+                if (currentSum > previousSum)
+                {
+                    increases++;
+                }
+            }
+
+            index = index % 3;
+        }
+    }
+
+    private static final class SumIncreaseCounter implements Consumer<String> {
+        private final Window window = new Window();
+
+        @Override
+        public void accept(String line) {
+            window.accept(Integer.parseInt(line));
+        }
+
+        public int increases() {
+            return window.increases;
+        }
+    }
+}
---
+++ src/test/java/net/digihippo/aoc/OneTest.java
@@ -0,0 +1,65 @@
+package net.digihippo.aoc;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OneTest
+{
+    @Test
+    void exampleInput() throws IOException
+    {
+        final String input =
+"""
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+""";
+        assertEquals(7, One.countIncreases(asInputStream(input)));
+    }
+
+    @Test
+    void realInput() throws IOException
+    {
+        System.out.println(One.countIncreases(this.getClass().getResourceAsStream("/one.txt")));
+    }
+
+    @Test
+    void partTwo() throws IOException
+    {
+        final String input =
+"""
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+""";
+        assertEquals(5, One.countSumIncreases(asInputStream(input)));
+    }
+
+    @Test
+    void partTwoForRealz() throws IOException {
+         System.out.println(One.countSumIncreases(this.getClass().getResourceAsStream("/one.txt")));
+    }
+
+    private static ByteArrayInputStream asInputStream(String input) {
+        return new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+    }
+}
