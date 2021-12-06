c3727b868e1e820819d89f7fa7eb75d20be12f39
byattj <byattj@lmax.com>
2021-12-06 09:52:56 +0000

    Save some allocations

--- src/main/java/net/digihippo/aoc/Six.java
+++ src/main/java/net/digihippo/aoc/Six.java
@@ -6,33 +6,28 @@ import java.util.Arrays;
 
 public class Six {
     public static long compute(int dayCount, InputStream stream) throws IOException {
-        long[] fishCount = new long[9];
+        final long[] fishCount = new long[9];
 
-        long[] finalFishCount = fishCount;
         Lines.processLines(stream, input -> {
             final String[] split = input.split(",");
             for (String part : split) {
                 final int timer = Integer.parseInt(part);
-                finalFishCount[timer]++;
+                fishCount[timer]++;
             }
         });
 
         for (int i = 0; i < dayCount; i++) {
-             fishCount = runDay(fishCount);
+             runDay(fishCount);
         }
 
         return Arrays.stream(fishCount).sum();
     }
 
-    private static long[] runDay(long[] timerCounts) {
-        long[] newTimerCounts = new long[9];
-
+    private static void runDay(long[] timerCounts) {
         long newEightFish = timerCounts[0];
 
-        System.arraycopy(timerCounts, 1, newTimerCounts, 0, newTimerCounts.length - 1);
-        newTimerCounts[6] += newEightFish;
-        newTimerCounts[8] += newEightFish;
-
-        return newTimerCounts;
+        System.arraycopy(timerCounts, 1, timerCounts, 0, timerCounts.length - 1);
+        timerCounts[6] += newEightFish;
+        timerCounts[8] = newEightFish;
     }
 }
