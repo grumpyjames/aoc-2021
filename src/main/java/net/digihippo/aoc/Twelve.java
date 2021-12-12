335f3c909688ee7572025aa6889571e2ba256525
James Byatt <grumpyjames@gmail.com>
2021-12-12 10:19:29 +0000

    Tidying up

--- src/main/java/net/digihippo/aoc/Twelve.java
+++ src/main/java/net/digihippo/aoc/Twelve.java
@@ -3,55 +3,47 @@ package net.digihippo.aoc;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
+import java.util.function.Supplier;
 
 public class Twelve {
     public static int pathCountUgh(InputStream stream) throws IOException {
         final Map<String, List<Path>> directedEdges = direct(readPaths(stream));
 
-        final List<HandyPath> interestingPaths = new ArrayList<>();
-
-        final String end = "end";
-        List<Path> backPaths = directedEdges.get("end");
-        for (Path p : backPaths) {
-            HandyPath hp = new Node(end, new Empty());
-            backTrack(p, hp, new HashSet<>(), directedEdges,
-                    interestingPaths, true);
-        }
-
-        for (HandyPath interestingPath : interestingPaths) {
-            StringBuilder sb = new StringBuilder();
-            interestingPath.describe(sb);
-            System.out.println(sb);
-        }
+        final List<HandyPath> interestingPaths =
+                findPaths(
+                        directedEdges,
+                        () -> new ChangeableVisitConditions(new HashSet<>(), true));
 
         return interestingPaths.size();
     }
 
     public static int pathCount(InputStream stream) throws IOException {
         final Map<String, List<Path>> directedEdges = direct(readPaths(stream));
+        List<HandyPath> paths = findPaths(
+                directedEdges,
+                () -> new ChangeableVisitConditions(new HashSet<>(), false));
 
+        return paths.size();
+    }
+
+    private static List<HandyPath> findPaths(
+            Map<String, List<Path>> directedEdges,
+            Supplier<ChangeableVisitConditions> s) {
         final List<HandyPath> interestingPaths = new ArrayList<>();
-
         final String end = "end";
         List<Path> backPaths = directedEdges.get("end");
+
         for (Path p : backPaths) {
             HandyPath hp = new Node(end, new Empty());
+            ChangeableVisitConditions conditions = s.get();
             backTrack(
                     p,
                     hp,
-                    new HashSet<>(),
                     directedEdges,
                     interestingPaths,
-                    false);
+                    conditions);
         }
-
-        for (HandyPath interestingPath : interestingPaths) {
-            StringBuilder sb = new StringBuilder();
-            interestingPath.describe(sb);
-            System.out.println(sb);
-        }
-
-        return interestingPaths.size();
+        return interestingPaths;
     }
 
     private static Map<String, List<Path>> direct(List<Path> input) {
@@ -77,23 +69,18 @@ public class Twelve {
     private static void backTrack(
             Path p,
             HandyPath bread,
-            Set<String> smallCavesVisited,
             Map<String, List<Path>> directedEdges,
             List<HandyPath> interestingPaths,
-            boolean secondVisitAllowed) {
+            ChangeableVisitConditions conditions) {
         String travellingTo = p.end;
-        HashSet<String> newVisited = new HashSet<>(smallCavesVisited);
         if (travellingTo.equals("start")) {
             HandyPath newPath = bread.add(travellingTo);
             interestingPaths.add(newPath);
         }
-        else if (travellingTo.equals("end")) {
-            //noinspection UnnecessaryReturnStatement
-            return;
-        }
-        else if (smallCave(travellingTo))
+        else
         {
-            if (newVisited.add(travellingTo))
+            Allowed allowed = conditions.performVisitationCheck(travellingTo);
+            if (allowed.allowed)
             {
                 List<Path> away = directedEdges.get(travellingTo);
                 for (Path path : away) {
@@ -101,38 +88,10 @@ public class Twelve {
                     backTrack(
                             path,
                             newPath,
-                            newVisited,
                             directedEdges,
                             interestingPaths,
-                            secondVisitAllowed);
+                            allowed.newConditions);
                 }
-            }
-            else if (secondVisitAllowed)
-            {
-                List<Path> away = directedEdges.get(travellingTo);
-                for (Path path : away) {
-                    HandyPath newPath = bread.add(travellingTo);
-                    backTrack(
-                            path,
-                            newPath,
-                            newVisited,
-                            directedEdges,
-                            interestingPaths,
-                            false);
-                }
-            }
-        }
-        else {
-            List<Path> away = directedEdges.get(travellingTo);
-            for (Path path : away) {
-                HandyPath newPath = bread.add(travellingTo);
-                backTrack(
-                        path,
-                        newPath,
-                        newVisited,
-                        directedEdges,
-                        interestingPaths,
-                        secondVisitAllowed);
             }
         }
     }
@@ -165,8 +124,43 @@ public class Twelve {
         }
     }
 
+    record Allowed(ChangeableVisitConditions newConditions, boolean allowed) {}
+
+    private record ChangeableVisitConditions(
+            HashSet<String> visited,
+            boolean secondVisitAllowed) {
+
+        Allowed performVisitationCheck(String s) {
+            if (s.equals("end")) {
+                return new Allowed(this, false);
+            }
+
+            if (smallCave(s)) {
+                HashSet<String> newThing = new HashSet<>(visited);
+                boolean newCave = newThing.add(s);
+                if (newCave) {
+                    return new Allowed(
+                            new ChangeableVisitConditions(
+                                    newThing,
+                                    secondVisitAllowed),
+                            true);
+
+                } else if (secondVisitAllowed) {
+                    return new Allowed(
+                            new ChangeableVisitConditions(
+                                    visited,
+                                    false),
+                            true);
+                } else {
+                    return new Allowed(this, false);
+                }
+            }
+
+            return new Allowed(this, true);
+        }
+    }
+
     private static boolean smallCave(String location) {
-        boolean notStartOrEnd = !location.equals("start") && !location.equals("end");
-        return notStartOrEnd && location.toLowerCase(Locale.ROOT).equals(location);
+        return location.toLowerCase(Locale.ROOT).equals(location);
     }
 }
