b35f818acf1800530dadf7523c4c86bfb3d928ef
byattj <byattj@lmax.com>
2021-12-23 13:21:41 +0000

    I'm the king of the world^H^H^H^H^H writing very slow dijkstra

--- src/main/java/net/digihippo/aoc/TwentyThree.java
+++ src/main/java/net/digihippo/aoc/TwentyThree.java
@@ -2,9 +2,7 @@ package net.digihippo.aoc;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class TwentyThree {
     public static Burrow emptyHalls(String[] corridors) {
@@ -17,14 +15,144 @@ public class TwentyThree {
         return Lines.parseLines(stream, new BurrowParser());
     }
 
-    sealed interface Move permits Exit, Entry {}
+    public static Burrow enactMove(Burrow b, Move move) {
+        String[] corridors = new String[4];
+
+        char[] hallway = new char[11];
+        System.arraycopy(b.hallway, 0, hallway, 0, b.hallway.length);
+
+        switch (move) {
+            case Switch s -> {
+                enactExit(b, corridors, hallway, s.exit);
+                hallway[s.entry.fromHallwayIndex] = '.';
+                for (int i = 0; i < corridors.length; i++) {
+                    if (i == s.entry.toRoomIndex) {
+                        final char[] chars = b.corridors[i].toCharArray();
+                        chars[s.entry.roomSlot] = s.entry.pod;
+                        corridors[i] = new String(chars);
+                    }
+                }
+            }
+            case Entry entry -> enactEntry(b, corridors, hallway, entry);
+            case Exit exit -> enactExit(b, corridors, hallway, exit);
+        }
+
+        return new Burrow(corridors, hallway);
+    }
+
+    private static void enactExit(Burrow b, String[] corridors, char[] hallway, Exit exit) {
+        hallway[exit.whereTo] = exit.pod;
+        for (int i = 0; i < corridors.length; i++) {
+            if (i == exit.roomIndex) {
+                final char[] chars = b.corridors[i].toCharArray();
+                chars[exit.roomSlot] = '.';
+                corridors[i] = new String(chars);
+            } else {
+                corridors[i] = b.corridors[i];
+            }
+        }
+    }
+
+    private static void enactEntry(Burrow b, String[] corridors, char[] hallway, Entry entry) {
+        hallway[entry.fromHallwayIndex] = '.';
+        for (int i = 0; i < corridors.length; i++) {
+            if (i == entry.toRoomIndex) {
+                final char[] chars = b.corridors[i].toCharArray();
+                chars[entry.roomSlot] = entry.pod;
+                corridors[i] = new String(chars);
+            } else {
+                corridors[i] = b.corridors[i];
+            }
+        }
+    }
+
+    public static long minimumCost(InputStream stream) throws IOException {
+        final Burrow burrow = Lines.parseLines(stream, new BurrowParser());
+
+        final TreeMap<Long, Set<Burrow>> frontier = new TreeMap<>();
+        final Set<Burrow> visited = new HashSet<>();
+        final Map<Burrow, Long> costs = new HashMap<>();
+
+        final Burrow finito = emptyHalls(new String[]{"AA", "BB", "CC", "DD"});
+
+        final List<Move> moves = validMoves(burrow);
+        for (Move move : moves) {
+            final Burrow value = enactMove(burrow, move);
+            frontier.computeIfAbsent(move.cost(), k -> new HashSet<>()).add(value);
+            costs.put(value, move.cost());
+        }
+
+        Optional<Map.Entry<Long, Set<Burrow>>> maybeEntry = Optional.of(frontier.firstEntry());
+        int routesExamined = 0;
+        while (maybeEntry.isPresent()) {
+            final Map.Entry<Long, Set<Burrow>> entry = maybeEntry.get();
+            final long costSoFar = entry.getKey();
+            final Set<Burrow> nextBurrows = entry.getValue();
+            @SuppressWarnings("OptionalGetWithoutIsPresent") final Burrow nextBurrow =
+                    nextBurrows.stream().filter(b -> !visited.contains(b)).findFirst().get();
+            if (visited.add(nextBurrow)) {
+                final List<Move> newMoves = validMoves(nextBurrow);
+                for (Move newMove : newMoves) {
+                    final Burrow buzza = enactMove(nextBurrow, newMove);
+                    if (buzza.equals(finito))
+                    {
+                        System.out.println("Found an ending");
+                    }
+                    final long newCost = costSoFar + newMove.cost();
+                    if (costs.containsKey(buzza)) {
+                        final long oldCost = costs.get(buzza);
+                        if (newCost < oldCost) {
+                            final Set<Burrow> frontierSet = frontier.get(oldCost);
+                            if (frontierSet != null) {
+                                frontierSet.remove(buzza);
+                                frontier.computeIfAbsent(newCost, k -> new HashSet<>()).add(buzza);
+                                costs.put(buzza, newCost);
+                            } else {
+                                System.out.println("Interesting");
+                            }
+                        }
+                    } else {
+                        costs.put(buzza, newCost);
+                        frontier.computeIfAbsent(newCost, k -> new HashSet<>()).add(buzza);
+                    }
+                }
+            }
+
+            final long minCostSoFar = costs.getOrDefault(finito, Long.MAX_VALUE);
+            maybeEntry = frontier
+                    .entrySet()
+                    .stream()
+                    .filter(e -> e.getKey() < minCostSoFar && e.getValue().stream().anyMatch(b -> !visited.contains(b)))
+                    .findFirst();
+
+            routesExamined++;
+            if (routesExamined % 10000 == 0)
+            {
+                System.out.println("Examined " + routesExamined + " routes, min cost so far is " + minCostSoFar);
+            }
+        }
+
+        return costs.get(finito);
+    }
+
+    sealed interface Move permits Exit, Entry, Switch {
+        long cost();
+    }
 
     record Entry(int fromHallwayIndex, char pod, int toRoomIndex, int roomSlot, long cost) implements Move {}
     record Exit(int roomIndex, int roomSlot, char pod, int whereTo, long cost) implements Move {}
+    record Switch(Exit exit, Entry entry) implements Move {
+        @Override
+        public long cost() {
+            return exit.cost + entry.cost;
+        }
+    }
 
     private static final long[] COSTS = new long[]{1, 10, 100, 1000};
     private static final int[] VALID_HALLWAY_POSITIONS = new int[] {0, 1, 3, 5, 7, 9, 10};
     private static final int[] HALLWAY_ADJACENTS = new int[] {2, 4, 6, 8};
+
+    static final int[] primes = new int[] {2, 3, 5, 7};
 
     record Burrow(String[] corridors, char[] hallway) {
         @Override
@@ -35,11 +163,18 @@ public class TwentyThree {
             return Arrays.equals(corridors, burrow.corridors) && Arrays.equals(hallway, burrow.hallway);
         }
 
+
         @Override
         public int hashCode() {
-            int result = Arrays.hashCode(corridors);
-            result = 31 * result + Arrays.hashCode(hallway);
-            return result;
+            int roomHash = 1;
+            for (int i = 0; i < corridors.length; i++) {
+                String corridor = corridors[i];
+                int prime = primes[i];
+                roomHash = roomHash * (prime * (corridor.charAt(0) - 'A'));
+                roomHash = roomHash * (prime * (corridor.charAt(1) - 'A'));
+            }
+
+            return 11 * roomHash + Arrays.hashCode(hallway);
         }
 
         @Override
@@ -72,6 +207,7 @@ public class TwentyThree {
 
         final List<Move> moves = new ArrayList<>();
 
+        // look for entries from the hallway to the corridor.
         for (int i = 0; i < burrow.hallway.length; i++) {
             char c = burrow.hallway[i];
 
@@ -79,6 +215,64 @@ public class TwentyThree {
             {
                 addEntryMoves(burrow, i, c, c - 'A', moves);
             }
+        }
+
+        // look for switches
+        for (char i = 0; i < corridors.length; i++) {
+            String corridor = corridors[i];
+            char top = corridor.charAt(0);
+            char bottom = corridor.charAt(1);
+
+            final int correctPod = 'A' + i;
+            if (top != '.' && top != correctPod)
+            {
+                int targetCorridorIndex = top - 'A';
+                long moveCost = COSTS[targetCorridorIndex];
+                final int newX = HALLWAY_ADJACENTS[targetCorridorIndex];
+                final int oldX = HALLWAY_ADJACENTS[i];
+                if (reachable(burrow.hallway, oldX, newX)) {
+                    final Exit exit = new Exit(i, 0, top, newX, moveCost * (1 + Math.abs(oldX - newX)));
+                    if (corridors[targetCorridorIndex].charAt(0) == '.' && corridors[targetCorridorIndex].charAt(1) == top) {
+                        moves.add(new Switch(
+                                exit,
+                                new Entry(newX, top, targetCorridorIndex, 0, moveCost)
+                        ));
+                    }
+                    else if (corridors[targetCorridorIndex].charAt(0) == '.' && corridors[targetCorridorIndex].charAt(1) == '.') {
+                        moves.add(new Switch(
+                                exit,
+                                new Entry(newX, top, targetCorridorIndex, 1, 2 * moveCost)
+                        ));
+                    }
+                }
+            }
+
+            if (top == '.' && bottom != '.' && bottom != correctPod)
+            {
+                final int targetCorridorIndex = bottom - 'A';
+                long moveCost = COSTS[targetCorridorIndex];
+                final int newX = HALLWAY_ADJACENTS[targetCorridorIndex];
+                final int oldX = HALLWAY_ADJACENTS[i];
+                if (reachable(burrow.hallway, oldX, newX)) {
+                    if (corridors[targetCorridorIndex].charAt(0) == '.' && corridors[targetCorridorIndex].charAt(1) == bottom) {
+                        moves.add(new Switch(
+                                new Exit(i, 1, bottom, newX, moveCost * (2 + Math.abs(oldX - newX))),
+                                new Entry(newX, bottom, targetCorridorIndex, 0, moveCost)
+                        ));
+                    }
+                    else if (corridors[targetCorridorIndex].charAt(0) == '.' && corridors[targetCorridorIndex].charAt(1) == '.') {
+                        moves.add(new Switch(
+                                new Exit(i, 1, bottom, newX, moveCost * (2 + Math.abs(oldX - newX))),
+                                new Entry(newX, bottom, targetCorridorIndex, 1, 2 * moveCost)
+                        ));
+                    }
+                }
+            }
+        }
+
+        if (!moves.isEmpty())
+        {
+            return moves;
         }
 
         for (char i = 0; i < corridors.length; i++) {
@@ -125,16 +319,17 @@ public class TwentyThree {
 
     private static void addEntryMoves(Burrow burrow, int i, char c, int index, List<Move> moves) {
 
-        if (reachable(burrow.hallway, 3, i)) {
+        final int targetX = HALLWAY_ADJACENTS[index];
+        final char targetPod = (char) ('A' + ((char) index));
+        if (targetPod == c && reachable(burrow.hallway, targetX, i)) {
             long moveCost = COSTS[index];
-            int roomOffset = HALLWAY_ADJACENTS[index];
             final String corridor = burrow.corridors[index];
 
-            int hMove = Math.abs(roomOffset - i);
+            int hMove = Math.abs(targetX - i);
 
-            if (corridor.charAt(1) == '.') {
+            if (corridor.charAt(1) == '.' && corridor.charAt(0) == '.') {
                 moves.add(new Entry(i, c, index, 1, (2 + hMove) * moveCost));
-            } else if (corridor.charAt(0) == '.') {
+            } else if (corridor.charAt(0) == '.' && corridor.charAt(1) == c) {
                 moves.add(new Entry(i, c, index, 0, (1 + hMove) * moveCost));
             }
         }
@@ -142,10 +337,15 @@ public class TwentyThree {
 
     private static class BurrowParser implements Lines.Parser<Burrow> {
         int count = 0;
+        char[] hallway;
         String[] corridors = new String[4];
 
         @Override
         public void onLine(String string) {
+            if (count == 1)
+            {
+                hallway = string.substring(1, string.length() - 1).toCharArray();
+            }
             if (count == 2)
             {
                 for (int i = 0; i < corridors.length; i++) {
@@ -165,7 +365,7 @@ public class TwentyThree {
 
         @Override
         public Burrow build() {
-            return emptyHalls(corridors);
+            return new Burrow(corridors, hallway);
         }
     }
 }
--- src/test/java/net/digihippo/aoc/TwentyThreeTest.java
+++ src/test/java/net/digihippo/aoc/TwentyThreeTest.java
@@ -9,15 +9,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TwentyThreeTest {
 
+    private final String exampleOne = """
+            #############
+            #...........#
+            ###B#C#B#D###
+              #A#D#C#A#
+              #########""";
+
     @Test
     void burrowParse() throws IOException {
-
-        final String amphipodes = """
-                #############
-                #...........#
-                ###B#C#B#D###
-                  #A#D#C#A#
-                  #########""";
 
 
         assertEquals(
@@ -25,7 +25,7 @@ class TwentyThreeTest {
                         new String[]{"BA", "CD", "BC", "DA"},
                         new char[]{'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}
                 ),
-                TwentyThree.parse(Inputs.asInputStream(amphipodes))
+                TwentyThree.parse(Inputs.asInputStream(exampleOne))
         );
     }
 
@@ -111,32 +111,7 @@ class TwentyThreeTest {
         final char[] hallway = ".A.........".toCharArray();
         final TwentyThree.Burrow burrow = new TwentyThree.Burrow(corridors, hallway);
 
-        final List<TwentyThree.Move> moves = List.of(
-                entry(1, 'A', 0, 0, 2),
-
-                exit(0, 1, 'A', 3, 3),
-                exit(0, 1, 'A', 5, 5),
-                exit(0, 1, 'A', 7, 7),
-                exit(0, 1, 'A', 9, 9),
-                exit(0, 1, 'A', 10, 10),
-
-                exit(1, 0, 'B', 3, 20),
-                exit(1, 0, 'B', 5, 20),
-                exit(1, 0, 'B', 7, 40),
-                exit(1, 0, 'B', 9, 60),
-                exit(1, 0, 'B', 10, 70),
-
-                exit(2, 0, 'B', 3, 40),
-                exit(2, 0, 'B', 5, 20),
-                exit(2, 0, 'B', 7, 20),
-                exit(2, 0, 'B', 9, 40),
-                exit(2, 0, 'B', 10, 50),
-
-                exit(3, 0, 'D', 3, 6000),
-                exit(3, 0, 'D', 5, 4000),
-                exit(3, 0, 'D', 7, 2000),
-                exit(3, 0, 'D', 9, 2000),
-                exit(3, 0, 'D', 10, 3000));
+        final List<TwentyThree.Move> moves = List.of(entry(1, 'A', 0, 0, 2));
 
         final List<TwentyThree.Move> validMoves = TwentyThree.validMoves(burrow);
 
@@ -175,6 +150,156 @@ class TwentyThreeTest {
         assertMovesMatch(moves, validMoves);
     }
 
+    @Test
+    void enactAnEntry() {
+        final String[] corridors = new String[] { "AA", "..", "CC", "DD" };
+        final char[] hallway = "B........B.".toCharArray();
+
+        TwentyThree.Burrow b = new TwentyThree.Burrow(corridors, hallway);
+
+        TwentyThree.Burrow newBurrow = TwentyThree.enactMove(b, entry(0, 'B', 1, 1, 60));
+
+        final String[] afterCorridors = new String[] { "AA", ".B", "CC", "DD" };
+        final char[] afterHallway = ".........B.".toCharArray();
+        TwentyThree.Burrow afterBurrow = new TwentyThree.Burrow(afterCorridors, afterHallway);
+
+        assertEquals(afterBurrow, newBurrow);
+    }
+
+    @Test
+    void enactAnExit() {
+        final String[] corridors = new String[] { "BA", "AB", "CC", "DD" };
+        final char[] hallway = "...........".toCharArray();
+
+        TwentyThree.Burrow b = new TwentyThree.Burrow(corridors, hallway);
+
+        TwentyThree.Burrow newBurrow = TwentyThree.enactMove(b, exit(1, 0, 'A', 3, 2));
+
+        final String[] afterCorridors = new String[] { "BA", ".B", "CC", "DD" };
+        final char[] afterHallway = "...A.......".toCharArray();
+        TwentyThree.Burrow afterBurrow = new TwentyThree.Burrow(afterCorridors, afterHallway);
+
+        assertEquals(afterBurrow, newBurrow);
+    }
+
+    @Test
+    void noMoreExitsIfThereIsAnEntryAvailable() {
+        final String[] corridors = new String[] { ".A", "CB", "BC", "DD" };
+        final char[] hallway = ".A..........".toCharArray();
+
+        TwentyThree.Burrow b = new TwentyThree.Burrow(corridors, hallway);
+        final List<TwentyThree.Move> moves = List.of(
+                entry(1, 'A', 0, 0, 2)
+        );
+
+        final List<TwentyThree.Move> validMoves = TwentyThree.validMoves(b);
+
+        assertMovesMatch(moves, validMoves);
+    }
+
+    @Test
+    void canGoDirectlyFromCorridorToCorridor() {
+        final String[] corridors = new String[] { "BA", "CD", ".C", "DA" };
+        final char[] hallway = "...B.......".toCharArray();
+
+        TwentyThree.Burrow b = new TwentyThree.Burrow(corridors, hallway);
+        final TwentyThree.Switch s = new TwentyThree.Switch(
+                exit(1, 0, 'C', 6, 300),
+                entry(6, 'C', 2, 0, 100));
+        final List<TwentyThree.Move> moves = List.of(
+                s
+        );
+
+        final List<TwentyThree.Move> validMoves = TwentyThree.validMoves(b);
+
+        assertMovesMatch(moves, validMoves);
+
+        final TwentyThree.Burrow moved = TwentyThree.enactMove(b, s);
+        assertEquals(
+                new TwentyThree.Burrow(new String[] { "BA", ".D", "CC", "DA" }, "...B.......".toCharArray()),
+                moved
+        );
+    }
+
+    @Test
+    void canGoDirectlyFromCorridorToCorridorMarkTwo() {
+        final String[] corridors = new String[] { "BA", "CD", "..", "DA" };
+        final char[] hallway = "...B......C".toCharArray();
+
+        TwentyThree.Burrow b = new TwentyThree.Burrow(corridors, hallway);
+        final List<TwentyThree.Move> moves = List.of(
+                new TwentyThree.Entry(
+                        10, 'C', 2, 1, 600
+                ),
+                new TwentyThree.Switch(
+                        exit(1, 0, 'C', 6, 300),
+                        entry(6, 'C', 2, 1, 200))
+        );
+
+        final List<TwentyThree.Move> validMoves = TwentyThree.validMoves(b);
+
+        assertMovesMatch(moves, validMoves);
+    }
+
+    @Test
+    void anotherMoveVariant() throws IOException {
+        final TwentyThree.Burrow burrow = TwentyThree.parse(Inputs.asInputStream("""
+                #############
+                #...B.......#
+                ###B#.#C#D###
+                  #A#D#C#A#
+                  #########"""));
+        final List<TwentyThree.Move> moves = TwentyThree.validMoves(burrow);
+
+        assertTrue(
+                moves.contains(new TwentyThree.Exit(1, 1, 'D', 5, 3000))
+        );
+    }
+
+    @Test
+    void yetAnotherMoveVariant() throws IOException {
+        final TwentyThree.Burrow burrow = TwentyThree.parse(Inputs.asInputStream("""
+                #############
+                #...B.D.....#
+                ###B#.#C#D###
+                  #A#.#C#A#
+                  #########"""));
+        final List<TwentyThree.Move> moves = TwentyThree.validMoves(burrow);
+
+        assertTrue(
+                moves.contains(new TwentyThree.Entry(3, 'B', 1, 1, 30))
+        );
+    }
+
+    @Test
+    void oneMoreMoveVariant() throws IOException {
+        final String input = """
+                #############
+                #.....D.D...#
+                ###.#B#C#.###
+                  #A#B#C#A#
+                  #########""";
+
+        final TwentyThree.Burrow burrow = TwentyThree.parse(Inputs.asInputStream(input));
+        final List<TwentyThree.Move> moves = TwentyThree.validMoves(burrow);
+
+        assertTrue(
+                moves.contains(new TwentyThree.Exit(3, 1, 'A', 9, 3))
+        );
+    }
+
+    @Test
+    void partOneExample() throws IOException {
+        assertEquals(
+                12521,
+                TwentyThree.minimumCost(Inputs.asInputStream(exampleOne)));
+    }
+
+    @Test
+    void partOne() throws IOException {
+        System.out.println(TwentyThree.minimumCost(Inputs.puzzleInput("twentythree.txt")));
+    }
+
     private TwentyThree.Entry entry(int fromHallwayIndex, char pod, int toRoomIndex, int roomSlot, long cost) {
         return new TwentyThree.Entry(fromHallwayIndex, pod, toRoomIndex, roomSlot, cost);
     }
@@ -183,7 +308,7 @@ class TwentyThreeTest {
         return TwentyThree.emptyHalls(corridors);
     }
 
-    private TwentyThree.Move exit(int roomIndex, int roomSlot, char pod, int whereTo, long cost)
+    private TwentyThree.Exit exit(int roomIndex, int roomSlot, char pod, int whereTo, long cost)
     {
         return new TwentyThree.Exit(roomIndex, roomSlot, pod, whereTo, cost);
     }
